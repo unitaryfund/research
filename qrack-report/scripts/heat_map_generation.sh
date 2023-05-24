46ba003d76665e9e3bf362eb59c98c9a32e56793
Dan Strano <stranoj@gmail.com>
2023-05-24 16:20:34 -0400

    Collect SDRP level 0 point

--- qrack-report/scripts/heat_map_generation.sh
+++ qrack-report/scripts/heat_map_generation.sh
@@ -2,7 +2,7 @@ for t in {1..10}; do
     for wroot in {6..8}; do
         w=$(( $wroot * $wroot ))
         for ((d=1; d<=$w; ++d)); do
-            for s in {1..80}; do
+            for s in {0..80}; do
                 python3 heat_map_generation.py --trial=$t --width=$w --depth=$d --sdrp=$s
             done
         done
