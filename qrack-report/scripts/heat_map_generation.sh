a810b41cd1a44ef7337bdcec03763b4cef6df9fc
Dan Strano <stranoj@gmail.com>
2023-05-26 22:04:20 -0400

    Qrack: nest trials deepest in benchmark script

--- qrack-report/scripts/heat_map_generation.sh
+++ qrack-report/scripts/heat_map_generation.sh
@@ -1,6 +1,6 @@
-for t in {1..10}; do
-    for w in {2..28}; do
-        for ((d=1; d<=$w; ++d)); do
+for w in {2..28}; do
+    for ((d=1; d<=$w; ++d)); do
+        for t in {0..9}; do
             # for s in {0..80}; do
             python3 heat_map_generation.py --trial=$t --width=$w --depth=$d --sdrp=0
             # done
