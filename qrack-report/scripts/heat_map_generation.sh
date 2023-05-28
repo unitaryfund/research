d62c9ff81ea7b5e812172719e865510e8d1ae5ba
Dan Strano <stranoj@gmail.com>
2023-05-28 13:15:04 -0400

    Qrack: ideal heatmap

--- qrack-report/scripts/heat_map_generation.sh
+++ qrack-report/scripts/heat_map_generation.sh
@@ -1,9 +1,9 @@
 for w in {2..28}; do
     for ((d=1; d<=$w; ++d)); do
         for t in {0..9}; do
-            # for s in {0..80}; do
-            python3 heat_map_generation.py --trial=$t --width=$w --depth=$d --sdrp=0
-            # done
+            for s in {0..80}; do
+                python3 heat_map_generation.py --trial=$t --width=$w --depth=$d --sdrp=s
+            done
         done
     done
 done
