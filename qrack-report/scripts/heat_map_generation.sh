2f985a093953812fe08902757f6963dc98587ae8
Dan Strano <stranoj@gmail.com>
2023-05-28 14:08:00 -0400

    Qrack: fix heat_map_generation.sh

--- qrack-report/scripts/heat_map_generation.sh
+++ qrack-report/scripts/heat_map_generation.sh
@@ -1,8 +1,12 @@
-for w in {2..28}; do
+for wroot in {5..8}; do
+    w=$(( wroot * wroot ))
     for ((d=1; d<=$w; ++d)); do
         for t in {0..9}; do
-            for s in {0..80}; do
+            for ((s=80; s>=0; s--)); do
                 python3 heat_map_generation.py --trial=$t --width=$w --depth=$d --sdrp=$s
+                if [ $? -ne 0 ]; then
+                    break
+                fi
             done
         done
     done
